62f1453fcfa1d4e2e38df3dc95804fd2fcd53c85
wdjmvf <channatu0@hotmail.com>
2020-04-15 11:08:16 +0700

    fix

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,18 @@
-/*
+/**
 * This is a package name
 */
 package com.example;
+
+/**
+ * This is a class.
+ */
+public class Hell {
+
+  /**
+   * This is a constructor.
+   */
+  public Hell() {
+
+  }
+
+}
